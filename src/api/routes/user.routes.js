14fe30289d66adeec68ccb43e3dca52e8a2f71f1
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-07 15:03:43 -0800

    improve: update route for users

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -3,7 +3,6 @@ const authController = require('./../controller/user.auth')
 const userController = require('./../controller/user.controller')
 
 const router = express.Router();
-
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 router.post('/forgotpassword', authController.forgotPassword)
