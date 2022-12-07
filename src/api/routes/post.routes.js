69d426ea1b985e8f38a51f5ba2320fe7e70ab920
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-07 14:58:39 -0800

    improve: update Post route

--- src/api/routes/post.routes.js
+++ src/api/routes/post.routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const postController = require('../controller/post.controller');
+const commentController = require('../controller/comment.controller');
+const authController = require('./../controller/user.auth');
 
 const router = express.Router();
+router.use(authController.authenticate);
 
 router.post('/', postController.createNewPost);
-router.get('/', postController.getAllPost)
-router.post('/:postId/like', postController.likePost)
+router.get('/', postController.getAllSharedPost);
+router.get('/draft', postController.getAllDraftPost);
+router.get('/:id', postController.getPost);
+router.patch('/:postId/like', postController.likePost);
+router.post('/:postId/comment', commentController.createComment);
+router.patch('/:postId/:commentId/like', commentController.likeComment);
+router.patch('/:id', postController.editPost);
+router.put('/:id', postController.updatePost);
+router.delete('/:id', postController.deletePost);
 
 module.exports = router;
