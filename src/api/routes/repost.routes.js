f5ad4cd5b6e49546eac80edb0880444c9853d515
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-07 11:46:54 -0800

    new: add route handler for repost

---
+++ src/api/routes/repost.routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const router = express.Router()
+const repostController = require('./../controller/repost.controller')
+const authController = require('./../controller/user.auth')
+
+router.post('/:id/repost', authController.authenticate, repostController.rePostAPost)
+module.exports = router;
