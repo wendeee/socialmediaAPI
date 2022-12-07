bb7a76e5ece48a5d15eea46b1678981bcc45e644
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-07 15:05:21 -0800

    improve: update configuration for database connection

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ require('dotenv').config();
 
 const DB = process.env.DATABASE_LOCAL;
 
-mongoose
-.connect(DB)
-.then((connect) => {
-    // console.log(connect.connections);
-    console.log('Connection to DB successful!')
-})
-
-
+const DB_CONNECTION = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log('Connection to DB successful!');
+  } catch (err) {
+    console.log('Connection to DB failed!');
+  }
+};
+DB_CONNECTION();
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-}) 
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
