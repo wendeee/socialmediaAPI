4c232315438d22adbb34c47d64a2ae89e8358dc1
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-09 18:22:13 -0800

    improve: improve delete functionality

--- src/api/controller/post.controller.js
+++ src/api/controller/post.controller.js
@@ -1,15 +1,15 @@
 /**
  * create a new post ✔
  * Get all post ✔
- * Get all timeline post - backlog
+ * Get all timeline post - get all shared post✔
  * get a single post  ✔
  * update a post  ✔
  * delete a post  ✔
  * like a shared post  ✔
- * comment on a shared post  
- * like a comment
+ * comment on a shared post
+ * like a comment - ✔
  * reshare a post  ✔
- * upload images -backlog
+ * upload images - ✔
  * upload videos -backlog
  * when you create a post, it is initially in a default draft state and does not appear on the user's timeline
  * when you create a post and update its state to 'shared', it appears on the user and his followers timeline.
@@ -18,21 +18,35 @@
 const User = require('../model/User');
 const Post = require('./../model/Post');
 const LikePost = require('./../model/likePost');
-
+const cloudinary = require('./../utils/cloudinary');
 //moment - date and time formatter package
 const moment = require('moment');
 
 //create a post
 exports.createNewPost = async (req, res, next) => {
-  try {
-    const newPost = req.body;
+  // let post;
+  const newPost = req.body;
 
-    //create an instance of the Post model object
-    const post = new Post({
-      author: req.user._id,
-      timestamps: moment().toDate(),
-      ...newPost,
-    });
+  try {
+    let post;
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      //create an instance of the Post model object
+      post = new Post({
+        author: req.user._id,
+        image: result.secure_url,
+        timestamps: moment().toDate(),
+        ...newPost,
+      });
+    } else {
+      //create an instance of the Post model object
+      post = new Post({
+        author: req.user._id,
+        timestamps: moment().toDate(),
+        ...newPost,
+      });
+    }
 
     //save created post to DB
     post.save((err) => {
@@ -50,16 +64,26 @@ exports.createNewPost = async (req, res, next) => {
       data: post,
     });
   } catch (err) {
+    console.log(err);
     return next(err);
   }
 };
 
-//get all post
+//get all post - fetches all timeline post
 exports.getAllSharedPost = async (req, res, next) => {
   try {
-    const posts = await Post.find({state: 'shared'}).populate('author', 'firstname lastname');
+    //find current user
+    const currentUser = await User.findById({ _id: req.user._id });
+    console.log(currentUser)
 
-    if (!posts) return res.status(200).json('No post has been shared yet');
+    //get all post of current user
+    const currentUserPosts = await Post.find({ author: req.user._id , state: 'shared'});
+
+    //get posts of current user followings
+    const followingsPosts = await Promise.all(currentUser.followings.map((followingId) =>
+      Post.find({ author: followingId , state: 'shared'})
+    ));
+    const posts = currentUserPosts.concat(...followingsPosts)
 
     res.status(200).json({
       status: 'success',
@@ -72,27 +96,27 @@ exports.getAllSharedPost = async (req, res, next) => {
 };
 
 //get all post in draft state
-exports.getAllDraftPost = async(req, res, next) => {
-  try{
-    const posts = await Post.find({state: 'draft'})
+exports.getAllDraftPost = async (req, res, next) => {
+  try {
+    const posts = await Post.find({ state: 'draft' });
 
-    if(!posts) return res.status(404).json({
-      status: 'success',
-      message: 'No post found'
-    })
-  
+    if (!posts)
+      return res.status(404).json({
+        status: 'success',
+        message: 'No post found',
+      });
+
     res.status(200).json({
       status: 'success',
       data: {
-        posts
-      }
-    })
-  }catch(err){
-    if(err) res.status(500)
-    return next(err.message)
+        posts,
+      },
+    });
+  } catch (err) {
+    if (err) res.status(500);
+    return next(err.message);
   }
- 
-}
+};
 
 //get a single post
 exports.getPost = async (req, res, next) => {
@@ -113,10 +137,12 @@ exports.getPost = async (req, res, next) => {
 
 //like or unlike a post
 exports.likePost = async (req, res, next) => {
-  //check if postid exist
+  try{
+     //check if postid exist
   const postToLike = await Post.findOne({ _id: req.params.postId });
 
-  if (postToLike.state === 'draft') return res.status(404).json('Post not found');
+  if (postToLike.state === 'draft')
+    return res.status(404).json('Post not found');
 
   //check the likepost collection to see if post has already been liked by the user
   let alreadyLikedPost = await LikePost.find();
@@ -165,30 +191,34 @@ exports.likePost = async (req, res, next) => {
     message: 'You liked this post',
     post: postToLike,
   });
+  }catch(err){
+    res.json(err)
+  }
+ 
 };
-
 
 //edit post
 exports.editPost = async (req, res, next) => {
   try {
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
-    if(!post) return res.status(404).json({
-      status: 'Fail',
-      message: 'Post not found'
-    })
+    if (!post)
+      return res.status(404).json({
+        status: 'Fail',
+        message: 'Post not found',
+      });
 
-    if(String(post.author) !== String(req.user._id)){
+    if (String(post.author) !== String(req.user._id)) {
       return res.status(401).json({
         status: 'Fail',
-        message: 'You can only edit a post created by you'
-      })
+        message: 'You can only edit a post created by you',
+      });
     }
 
-
     //edit only body of post and no other property
-    if (!req.body.post) return res.status(400).json('You can only edit the post body');
-    
+    if (!req.body.post)
+      return res.status(400).json('You can only edit the post body');
+
     const postToEdit = await Post.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
@@ -205,18 +235,19 @@ exports.editPost = async (req, res, next) => {
 exports.updatePost = async (req, res, next) => {
   try {
     //1). find the post using the id
-    const post = await Post.findById(req.params.id)
+    const post = await Post.findById(req.params.id);
 
-    if(!post) return res.status(404).json({
-      status: 'Fail',
-      message: 'Post not found'
-    })
+    if (!post)
+      return res.status(404).json({
+        status: 'Fail',
+        message: 'Post not found',
+      });
 
-    if(String(post.author) !== String(req.user._id)){
+    if (String(post.author) !== String(req.user._id)) {
       return res.status(401).json({
         status: 'Fail',
-        message: 'You can only update a post created by you'
-      })
+        message: 'You can only update a post created by you',
+      });
     }
 
     const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
@@ -233,10 +264,21 @@ exports.updatePost = async (req, res, next) => {
 
 //delete post
 exports.deletePost = async (req, res, next) => {
-await Post.findByIdAndRemove(req.params.id);
-  res.status(204).json({
-    status: 'success',
-    message: 'post deleted sucessfully',
-  });
+  try{
+    const post = await Post.findById(req.params.id)
+    //delete post from user's 'posts' array in user model
+    const postByUser = await User.findById(req.user._id);
+    postByUser.posts.pull(post._id);
+    await postByUser.updateOne({ posts: postByUser.posts });
+   // user.posts
+   await Post.findByIdAndRemove(req.params.id);
+ 
+   res.status(200).json({
+     status: 'success',
+     message: 'post deleted sucessfully',
+   });
+  }catch(err){
+    return next(err)
+  }
+ 
 };
-
