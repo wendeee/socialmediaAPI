789f46e96a18227e7300a2d7f1b7b7241abb9713
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-09 19:46:25 -0800

    improve: add multer config middleware to upload image

--- src/api/routes/post.routes.js
+++ src/api/routes/post.routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const postController = require('../controller/post.controller');
 const commentController = require('../controller/comment.controller');
 const authController = require('./../controller/user.auth');
-
+const uploads = require('./../utils/multer');
 const router = express.Router();
 router.use(authController.authenticate);
 
-router.post('/', postController.createNewPost);
+router.post('/', uploads.single('image'), postController.createNewPost);
 router.get('/', postController.getAllSharedPost);
 router.get('/draft', postController.getAllDraftPost);
 router.get('/:id', postController.getPost);
