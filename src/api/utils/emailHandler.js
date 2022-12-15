95ee0d0d905eaa0a1edb4c41e063430e4d2277ff
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-14 23:13:50 -0800

    new: add send email functionality using nodemailer and mailtrap

--- src/api/utils/emailHandler.js
+++ src/api/utils/emailHandler.js
@@ -1,7 +1,25 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 
-const sendEmail = options => {
-    //1) Create a transporter
-    //2) Define email options
-    //3) Send email
-}
+const sendEmail = async (options) => {
+  //create transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  //define mail options
+  const mailOptions = {
+    from: '"Tame" <update@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  //send the email
+  await transporter.sendMail(mailOptions);
+};
+module.exports = sendEmail;
