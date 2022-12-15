1acce1f0911d12ff9c7d252ed35feca0d4ee397e
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-14 23:15:15 -0800

    new: add forgot password and reset password functionality

--- src/api/controller/user.auth.js
+++ src/api/controller/user.auth.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 const User = require('./../model/User');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
+const sendEmail = require('./../utils/emailHandler');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -108,21 +110,86 @@ exports.authenticate = async (req, res, next) => {
     req.user = currentUser;
     next();
   } catch (err) {
-    res.json(err)
-  
+    res.json(err);
   }
 };
 
-// exports.forgotPassword = async (req, res, next) => {
-//   //1. Find user in Db using provided credential
-//   const user = await User.findOne({ email: req.body.email });
-//   if (!user) return res.status(404).json('User not found!');
+exports.forgotPassword = async (req, res, next) => {
+  //1. Find user in Db using email
+  const user = await User.findOne({ email: req.body.email });
 
-//   //2.Generate a random reset token
-//   const resetToken = user.createPasswordResetToken();
-//   await user.save({ validateBeforeSave: false }); //validatBeforeSave: false will turn off all validations before saving to db
+  if (!user) return res.status(404).json('User not found!');
 
-//   //3.send token to the user's email - backlog
-// };
+  //2.Generate a random reset token by calling the generatePasswordResetToken() method
+  const resetToken = await user.generatePasswordResetToken();
+  await user.save({ validateBeforeSave: false }); //validatBeforeSave: false will turn off all validations before saving user obj to db
 
-//exports.resetPassword
+  //3.create reset link and send token to the user's email using sgMail
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/resetPassword/${resetToken}`;
+
+  const message = `Hello ${user.username},\n 
+  A request has been received to change your password for your LinkUp account.\n
+  Send a PATCH request with your new password and passwordConfirm to this link:\n ${resetURL}\n If you did not initiate this request, please ignore and your password will remain unchaged`;
+
+  // console.log(mailOptions)
+  //send email to user
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your Password Reset Token',
+      message,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'A reset token has been sent to your email',
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpire = undefined;
+    console.log(err.message);
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new Error('An error occurred while sending email. Please try again')
+    );
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  //hash token and compare with hashed token in the db
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  //find user with hashed token
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpire: { $gt: Date.now() },
+  });
+
+  //if token has expired
+  if (!user) return next(new Error('User not found or token has expired'));
+
+  //if user exist, set new password
+  const { password, passwordConfirm } = req.body;
+  user.password = password;
+  user.passwordConfirm = passwordConfirm;
+
+  //delete passwordResetToken and passwordResetExpire fields
+  user.passwordResetToken = undefined;
+  user.passwordResetExpire = undefined;
+
+  //save new changes to db
+  await user.save();
+
+  //login user and assign jwt token
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
