1033a52c4474eeb2af7c78a0c99955d0d0625480
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-15 07:54:06 -0800

    improve: add forgotPassword and resetPassword route

--- src/api/routes/user.auth.route.js
+++ src/api/routes/user.auth.route.js
@@ -3,6 +3,6 @@ const authController = require('./../controller/user.auth');
 const router = express();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.post('/forgotPassword', authController.forgotPassword)
-router.patch('/resetPassword/:token', authController.resetPassword)
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 module.exports = router;
