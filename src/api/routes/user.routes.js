753afa3ee6c9fb5d95fdade1fddcb11ce78fc743
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-15 07:55:23 -0800

    improve: format code better with prettier

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -3,13 +3,36 @@ const authController = require('./../controller/user.auth');
 const userController = require('./../controller/user.controller');
 
 const router = express.Router();
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.patch('/updateprofile', userController.updateMe);
-router.delete('/deleteprofile', userController.deleteMe);
-router.patch('/:tofollowId/follow', userController.follow);
-router.patch('/:unfollowid/unfollow', userController.unfollow);
-router.get('/followers', userController.getFollowers);
-router.get('/followings', userController.getFollowings);
+
+router.patch(
+  '/updateprofile',
+  authController.authenticate,
+  userController.updateMe
+);
+router.delete(
+  '/deleteprofile',
+  authController.authenticate,
+  userController.deleteMe
+);
+router.patch(
+  '/:tofollowId/follow',
+  authController.authenticate,
+  userController.follow
+);
+router.patch(
+  '/:unfollowid/unfollow',
+  authController.authenticate,
+  userController.unfollow
+);
+router.get(
+  '/followers',
+  authController.authenticate,
+  userController.getFollowers
+);
+router.get(
+  '/followings',
+  authController.authenticate,
+  userController.getFollowings
+);
 
 module.exports = router;
