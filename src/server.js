58c10556777b5a441dd5c05d6eebf26fe4246cbe
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-15 10:45:01 -0800

    improve: change db connection logic

--- src/server.js
+++ src/server.js
@@ -2,17 +2,23 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE_ATLAS;
 
-const DB_CONNECTION = async () => {
-  try {
-    await mongoose.connect(DB);
-    console.log('Connection to DB successful!');
-  } catch (err) {
-    console.log('Connection to DB failed!');
-  }
-};
-DB_CONNECTION();
+// const DB_CONNECTION = async () => {
+//   try {
+//      mongoose.connect(DB);
+//     console.log('Connection to DB successful!');
+//   } catch (err) {
+//     console.log('Connection to DB failed!');
+//   }
+// };
+
+mongoose
+  .connect(process.env.DATABASE_ATLAS)
+  .then(() => console.log('Connection to DB successful'))
+  .catch((err) => console.log('Connection to DB failed', err));
+
+// DB_CONNECTION();
 
 const PORT = process.env.PORT || 3000;
 
